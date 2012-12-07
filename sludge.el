4cb41bd99d78c532a6edf87d22bc373ec54aa8e1
Alex Plotnick <shrike@netaxs.com>
2012-12-07 00:13:24 -0500

    Check for live process before making request.

--- sludge.el
+++ sludge.el
@@ -209,6 +209,8 @@ connection (i.e., the one from which all others will be cloned)."
   (process-send-string process (format "%S\n" request)))
 
 (defun sludge-async-request (process code args ok &optional err)
+  (unless (and process (process-live-p process))
+    (error "Invalid SLUDGE process"))
   (let ((tag (sludge-tag-counter process)))
     (sludge-set-callbacks process code tag ok err)
     (sludge-send-request process (apply #'make-sludge-request code tag args))))
